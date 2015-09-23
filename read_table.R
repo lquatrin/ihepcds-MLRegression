0f089bdb2863b8c01ae0908fa347993eb1d347f2
Leonardo Quatrin Campagnolo <campagnolo.lq@gmail.com>
2015-09-23 19:41:12 -0300

    table e data

--- read_table.R
+++ read_table.R
@@ -1,8 +1,21 @@
 rm(list=ls())
+StringsAsFactors = FALSE
 
-stringsAsFactors = FALSE
+HousehouldPowerConsumption = read.table("C:/Users/lquatrin/Downloads/household_power_consumption/household_power_consumption.txt", header=T, sep=";", as.is = !StringsAsFactors)
 
-dataset = read.table("C:/Users/lquatrin/Downloads/household_power_consumption/household_power_consumption.txt", header=T, sep=";", as.is = !stringsAsFactors)
+DateActPowerHousehouldPowerConsumption = subset(HousehouldPowerConsumption, select = c(1,3))
 
-#n??mero de linhas do dataset
-nrow(dataset)
+NADateActPowerHousehouldPowerConsumption = DateActPowerHousehouldPowerConsumption[DateActPowerHousehouldPowerConsumption$Global_active_power != '?',]
+
+FilteredHousehouldPowerConsumption = NADateActPowerHousehouldPowerConsumption
+FilteredHousehouldPowerConsumption$Global_active_power = as.numeric(FilteredHousehouldPowerConsumption$Global_active_power)
+
+#number of dataset's rows
+#nrow(dataset)
+
+ReadDataFunc <- function(){
+  print('hello')
+  print('hello2')
+  print('hello3')
+  print('hello4')
+}
