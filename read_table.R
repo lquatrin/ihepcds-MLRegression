de055d558e886c9bbb55774565e890148780dfcb
Leonardo Quatrin Campagnolo <lennom@gmail.com>
2015-09-24 13:30:14 -0300

    heh

--- read_table.R
+++ read_table.R
@@ -1,21 +1,27 @@
+#setwd("E:/GitHub/ihepcds-MLRegression")
+#setwd("C:/Users/lquatrin/GitHub/ihepcds-MLRegression")
+
 rm(list=ls())
 StringsAsFactors = FALSE
+print("Pre Processing Started...")
 
-HousehouldPowerConsumption = read.table("C:/Users/lquatrin/Downloads/household_power_consumption/household_power_consumption.txt", header=T, sep=";", as.is = !StringsAsFactors)
+HousehouldPowerConsumption = read.table("household_power_consumption.txt", header=T, sep=";", as.is = !StringsAsFactors)
+print("HousehouldPowerConsumption done...")
 
 DateActPowerHousehouldPowerConsumption = subset(HousehouldPowerConsumption, select = c(1,3))
+print("DateActPowerHousehouldPowerConsumption done...")
 
 NADateActPowerHousehouldPowerConsumption = DateActPowerHousehouldPowerConsumption[DateActPowerHousehouldPowerConsumption$Global_active_power != '?',]
+print("NADateActPowerHousehouldPowerConsumption done...")
 
 FilteredHousehouldPowerConsumption = NADateActPowerHousehouldPowerConsumption
 FilteredHousehouldPowerConsumption$Global_active_power = as.numeric(FilteredHousehouldPowerConsumption$Global_active_power)
+print("FilteredHousehouldPowerConsumption done...")
 
-#number of dataset's rows
-#nrow(dataset)
+NumericHousehouldPowerConsumption = FilteredHousehouldPowerConsumption
+NumericHousehouldPowerConsumption$Date = as.numeric(as.POSIXct(NumericHousehouldPowerConsumption$Date, format="%d/%m/%y"))
+print("NumericHousehouldPowerConsumption done...")
 
-ReadDataFunc <- function(){
-  print('hello')
-  print('hello2')
-  print('hello3')
-  print('hello4')
-}
+print("Pre Processing finished")
+
+#plot(NumericHousehouldPowerConsumption$Date, NumericHousehouldPowerConsumption$Global_active_power)
